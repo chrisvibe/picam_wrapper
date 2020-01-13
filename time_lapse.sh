34c3d0b466e9d496f71f7fbc575d09fd03e5ee59
chrisvibe <vibechris@gmail.com>
2020-01-13 18:57:52 +0100

    checkpoint

--- time_lapse.sh
+++ time_lapse.sh
@@ -1,13 +1,47 @@
+#!/bash/bin
+
+get_pics()
+{
 mkdir -p pics
-rsync --rsh=ssh -u pi@192.168.1.227:/home/pi/picam_wrapper/pics/* ./pics
+# rsync --rsh=ssh -u pi@192.168.1.227:/home/pi/picam_wrapper/pics/* ./pics
+rsync -avz --files-from=image_list.txt pi@192.168.1.227:/home/pi/picam_wrapper/pics ./pics
+}
+
+change_pic_names()
+{
 for f in pics/*; do mv "$f" $(echo $f | tr -d "-"); done
 for f in pics/*; do mv "$f" $(echo $f | tr -d "_"); done
+}
 
-fPattern=./pics/*.jpg
+reduce_time_frame()
+{
+for f in pics/*
+do
+    x=$(echo $f | cut -c 14-15) # hour
+
+    if [ $x -ge 7 ] && [ $x -le 15 ]
+    then
+       cp $f ./pics2/ 
+    fi
+done
+}
+
+fPattern=./pics2/*.jpg
 mFile=time_lapse.mp4
-sFile=enya.mp3
+# sFile=enya.mp3
+sFile=gymnopedies.mp3
 
-# ffmpeg -pattern_type glob -i "$fPattern" -i $sFile -framerate 20 -tune film $mFile
+make_time_lapse()
+{
+rm $mFile || true 
+ffmpeg -pattern_type glob -i "$fPattern" -i $sFile -framerate 1000 -tune film $mFile
+
+playback
+}
+
+make_looped_time_lapse()
+# for some reason changing the filenames between steps helps fix bugs
+{
 ffmpeg -pattern_type glob -i "$fPattern" -framerate 20 -tune film temp1.mp4 
 
 # build concat list
@@ -22,7 +56,6 @@ ffmpeg -f concat -i list.txt -c copy temp2.mp4
 # slow down vid
 ffmpeg -i temp2.mp4 -filter:v "setpts=3.0*PTS" temp3.mp4
 
-
 # add sound to looped video
 ffmpeg -i temp3.mp4 -i $sFile -codec copy temp4.mp4 
 
@@ -33,11 +66,20 @@ rm temp2.mp4
 rm temp3.mp4
 mv temp4.mp4 $mFile
 
-# playback
+playback
+}
+
+playback()
+{
 cvlc --fullscreen $mFile vlc://quit
+}
+
+# get_pics
+# change_pic_names
+# reduce_time_frame
+# make_time_lapse
 
 # Useful?: 
-# for some reason changing the filenames between steps helps fix bugs
 # cvlv = vlc -I dummy 
 # cvlc --no-video $mFile --sout="#std{access=file,mux=mp4,dst=$mFile.mpg}" vlc://quit
 # ffmpeg -i $mFile -loop 5 timeLapse.gif
