39809b6b28a6216e3bf10c468fe5c6b35777ef9d
chrisvibe <vibechris@gmail.com>
2020-02-15 00:11:29 +0100

    small fix - tested and works

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,4 +1,7 @@
+rm -fr picam_venv
 python3 -m venv picam_venv
+. picam_venv/bin/activate
 pip3 install -r requirements.txt
-echo "./picam_vevn/bin/activate (to use environment)"
+echo -e "\n------------------------------------------------------------\n"
+echo ". picam_venv/bin/activate (to use environment)"
 echo "deactivate (to tear down environment)"
--- take_picture.sh
+++ take_picture.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./picam_venv/bin/activate
+. picam_venv/bin/activate
 python3 -c 'import take_picture as tp; tp.take_picture()'
 deactivate
 # python3 -c 'import take_picture as tp; tp.take_picture(flash_mode=True)'
