b0db2af95bd1b2bfda86e63907b6f5bfa1066026
chrisvibe <vibechris@gmail.com>
2020-02-23 14:47:02 +0100

    added pics folder automatically

--- setup_environment.sh
+++ setup_environment.sh
@@ -2,6 +2,7 @@ rm -fr picam_venv
 python3 -m venv picam_venv
 . picam_venv/bin/activate
 pip3 install -r requirements.txt
+mkdir -p pics
 echo -e "\n------------------------------------------------------------\n"
 echo ". picam_venv/bin/activate (to use environment)"
 echo "deactivate (to tear down environment)"
